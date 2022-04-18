8ec374b54cf12acb83f84bae3af0e78ff688c72d
rhea-sidana-TW <rhea.sidana@thoughtworks.com>
2022-04-18 13:21:19 +0530

    Trello-#29 | [Rhea/Ragul] | Refactor conditions ChangePasswordService for execute function.

--- src/main/java/com/booking/users/ChangePasswordService.java
+++ src/main/java/com/booking/users/ChangePasswordService.java
@@ -6,7 +6,7 @@ import com.booking.users.repository.model.User;
 import com.booking.users.view.model.ChangePasswordRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+
 import java.util.regex.Pattern;
 
 @Service
@@ -21,23 +21,45 @@ public class ChangePasswordService {
     public void execute(ChangePasswordRequest changePasswordRequest, String userName) throws ChangePasswordException {
         String newPassword = changePasswordRequest.getNewPassword();
         String confirmNewPassword = changePasswordRequest.getConfirmNewPassword();
-        if (!newPassword.equals(confirmNewPassword)) {
-            throw new ChangePasswordException("New password does not match confirm new password");
-        }
-        if (!Pattern.matches(PASSWORD_PATTERN, newPassword)) {
-            throw new ChangePasswordException("New password does not match the pattern");
-        }
-        if (changePasswordRequest.getOldPassword().equals(newPassword)) {
-            throw new ChangePasswordException("New password is same as old password");
-        }
+
+        newPasswordNotEqualToConfirmNewPassword(newPassword, confirmNewPassword);
+
+        newPasswordNotMathcesWithPattern(newPassword);
+
+        newPasswordShouldNotBeEqualToOldPassword(changePasswordRequest, newPassword);
+
         User user = userRepository.findByUsername(userName).get();
-        if (!user.getPassword().equals(changePasswordRequest.getOldPassword())) {
-            throw new ChangePasswordException("Old password is wrong");
-        }
+
+        oldPasswordNotMatchesWithUserPassword(user, changePasswordRequest);
 
         user.setPassword(newPassword);
 
         userRepository.save(user);
+
+    }
+
+    private void oldPasswordNotMatchesWithUserPassword(User user, ChangePasswordRequest changePasswordRequest) throws ChangePasswordException {
+        if (!user.getPassword().equals(changePasswordRequest.getOldPassword())) {
+            throw new ChangePasswordException("Old password is wrong");
+        }
+    }
+
+    private void newPasswordShouldNotBeEqualToOldPassword(ChangePasswordRequest changePasswordRequest, String newPassword) throws ChangePasswordException {
+        if (changePasswordRequest.getOldPassword().equals(newPassword)) {
+            throw new ChangePasswordException("New password is same as old password");
+        }
+    }
+
+    private void newPasswordNotMathcesWithPattern(String newPassword) throws ChangePasswordException {
+        if (!Pattern.matches(PASSWORD_PATTERN, newPassword)) {
+            throw new ChangePasswordException("New password does not match the pattern");
+        }
+    }
+
+    void newPasswordNotEqualToConfirmNewPassword(String newPassword, String confirmNewPassword) throws ChangePasswordException {
+        if (!newPassword.equals(confirmNewPassword)) {
+            throw new ChangePasswordException("New password does not match confirm new password");
+        }
     }
 
 }
