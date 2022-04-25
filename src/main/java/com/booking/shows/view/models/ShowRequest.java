39def79e8389b6a8de01942d9a1d00912c6786eb
thejaswini.bk <thejaswini.bk@thoughtworks.com>
2022-04-25 21:04:26 +0530

    Trello-#38 | [Thejaswini/Ragul] | Add . Not null annotions to request

--- src/main/java/com/booking/shows/view/models/ShowRequest.java
+++ src/main/java/com/booking/shows/view/models/ShowRequest.java
@@ -5,29 +5,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.sql.Date;
 
 public class ShowRequest {
+
+    @NotNull
     @JsonProperty
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @ApiModelProperty(name = "date", value = "Date of booking (yyyy-MM-dd)", dataType = "java.lang.String", required = true, example = "2020-01-01", position = 1)
-    private  Date date;
+    private Date date;
 
+    @NotNull
     @JsonProperty
     @ApiModelProperty(name = "slot", value = "slot", required = true, position = 2)
-    private  int slotId;
+    private int slotId;
 
+    @NotNull
     @JsonProperty
     @DecimalMin(value = "0.0", inclusive = false, message = "Cost should be greater than {value}")
-    @Digits(integer = 4, fraction = 2, message = "Cost can have at most {integer} integral digits, and {fraction} fractional digits")
     @ApiModelProperty(name = "cost", value = "cost of movie", required = true, position = 3)
-    private  BigDecimal cost;
+    private BigDecimal cost;
 
+    @NotNull
     @JsonProperty
     @ApiModelProperty(name = "movie id", value = "The movie id", required = true, position = 4)
-    private  String movieId;
+    private String movieId;
 
     public Date getDate() {
         return date;
