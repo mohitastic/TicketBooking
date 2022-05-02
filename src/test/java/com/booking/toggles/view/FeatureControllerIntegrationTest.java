9d885b9c1ad9cafa12c83e392f4fa8757caa6e90
thejaswini.bk <thejaswini.bk@thoughtworks.com>
2022-05-02 19:44:20 +0530

    Trello#2 | [Thejaswini/Hardik] | Add. integration test for Feature Controller

---
+++ src/test/java/com/booking/toggles/view/FeatureControllerIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.booking.toggles.view;
+
+import com.booking.App;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = App.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@WithMockUser
+public class FeatureControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void retrieveAllExistingMoviesWhenFeatureIsEnabled() throws Exception {
+        mockMvc.perform(get("/features"))
+                .andExpect(status().isOk());
+    }
+}
