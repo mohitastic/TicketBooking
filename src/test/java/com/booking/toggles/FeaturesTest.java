aa706c75d663e3dc01bdd8ea5c4e2ec5eff990cc
thejaswini.bk <thejaswini.bk@thoughtworks.com>
2022-05-03 11:51:02 +0530

    Trello#2 | [Thejaswini/Hardik] |Modify . test for Features class

--- src/test/java/com/booking/toggles/FeaturesTest.java
+++ src/test/java/com/booking/toggles/FeaturesTest.java
@@ -2,6 +2,8 @@ package com.booking.toggles;
 
 import com.booking.App;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.togglz.junit5.AllDisabled;
 import org.togglz.junit5.AllEnabled;
@@ -9,7 +11,23 @@ import org.togglz.junit5.AllEnabled;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = App.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class FeaturesTest {
+    @Test
+    @AllEnabled(Features.class)
+    void allEnabled() {
+        assertTrue(Features.MOVIE_SCHEDULE.isActive());
+        assertTrue(Features.VIEW_USER_PROFILE.isActive());
+        assertTrue(Features.CUSTOMER_SIGN_UP.isActive());
+        assertTrue(Features.CHANGE_PASSWORD.isActive());
+    }
 
+    @Test
+    @AllDisabled(Features.class)
+    void allDisabled() {
+        assertFalse(Features.MOVIE_SCHEDULE.isActive());
+        assertFalse(Features.VIEW_USER_PROFILE.isActive());
+        assertFalse(Features.CUSTOMER_SIGN_UP.isActive());
+        assertFalse(Features.CHANGE_PASSWORD.isActive());
+    }
 }
